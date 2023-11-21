78c45a2e58e825b87fec89eabd5d94266a8df29d
Stray Cat <130251613+KucingTetangga@users.noreply.github.com>
2023-11-21 19:15:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pyodbc
 server = 'kil-thedb.database.windows.net'
 database = 'dbkiller'
 username = 'lucy'
-password = '{Darklatias2000}'
+password = '{password}'
 driver= '{ODBC Driver 18 for SQL Server}'
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
